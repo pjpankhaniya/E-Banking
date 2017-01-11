6bcad9898eab91d9e98deb54f81f987bc5734c81
IBM <IBM@IBM003-PBTRATN.in.ibm.com>
2017-01-11 10:59:37 +0530

    Comment

--- BankSystem/src/controler/Admin.java
+++ BankSystem/src/controler/Admin.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-//Comment
+//Commented here
 /**
  * Servlet implementation class Admin
  */
