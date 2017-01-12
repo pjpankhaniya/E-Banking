ed5f27b4529e00be87423a9f5cc4de19dc14c872
IBM <IBM@IBM679-PBTRATX.in.ibm.com>
2017-01-12 12:09:29 -0500

    yet to do transcation opearation

--- BankSystem/src/controler/Transfer.java
+++ BankSystem/src/controler/Transfer.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/Transfer")
 public class Transfer extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+     //not implemented yet  
     /**
      * @see HttpServlet#HttpServlet()
      */
